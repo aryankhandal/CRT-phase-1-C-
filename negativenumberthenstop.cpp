c4c7a87f85f8e4bacf97bdae1703652dcfb39c63
aryankhandal <aryankhandal0801@gmail.com>
2026-05-02 14:27:31 +0530

    added new file

---
+++ negativenumberthenstop.cpp
@@ -0,0 +1,18 @@
+//in this code we will take input from user continuously if inout number comes nagative then it stops 
+//and print the sum of all the positive numbers entered by user
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    int sum = 0;
+    cout << "Enter numbers (enter negative number to stop): " << endl;
+    while (true) {
+        cin >> num;
+        if (num < 0) {
+            break;  
+        }
+   sum += num; 
+    }
+    cout << "Sum of positive numbers = " << sum << endl;
+    return 0;
+}
