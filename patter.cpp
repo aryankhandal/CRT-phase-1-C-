c87b9028466b5358b355e3a08025c48be4c2dbe4
aryankhandal <aryankhandal0801@gmail.com>
2026-05-06 15:09:44 +0530

    "Updated subproject and day1 with dirty commits"

---
+++ patter.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int rows;
+    int i, j;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    for(int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
